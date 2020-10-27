2cf343389d16d1cff275b9b18d166579985833d8
EEdwardsA <ecomstockedwards@gmail.com>
2020-10-27 11:26:39 -0400

    replace setup.py filename with underscores instead of dashes

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="lambdata-eedwardsa", # Replace with your own username
-    version="0.0.1",
+    name="lambdata_eedwardsa", # Replace with your own username
+    version="0.0.2",
     author="EEdwardsA",
     description="A collection of data science functions",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/EEdwardsA/lambdata",
     packages=setuptools.find_packages(),
-    install_requires=REQUIRED
+    install_requires=REQUIRED,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
