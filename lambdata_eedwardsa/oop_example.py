1f8adf02355e7f2f97dcd6451e17fde1a706707d
EEdwardsA <ecomstockedwards@gmail.com>
2020-10-27 18:05:39 -0400

    pep8 style changes

--- lambdata_eedwardsa/oop_example.py
+++ lambdata_eedwardsa/oop_example.py
@@ -3,8 +3,10 @@
 import pandas as pd 
 import numpy as np 
 
+
 class BareMinimumClass:
     pass
+
 
 class Complex:
     def __init__(self, realpart, imagpart):
@@ -14,9 +16,11 @@ class Complex:
         """
         self.r = realpart
         self.i = imagpart
-    def add(self,other_complex):
+
+    def add(self, other_complex):
         self.r += other_complex.r
         self.i += other_complex.i
+
 
 class SocialMediaUser:
     def __init__(self, name, location, upvotes=0):
@@ -30,10 +34,11 @@ class SocialMediaUser:
     def is_popular(self):
         return self.upvotes > 100
 
+
 class MyDataFrame(pd.DataFrame):
     def num_cells(self):
         return self.shape[0] * self.shape[1]
-        
+
 
 class Animal:
     """General representation of animals"""
@@ -41,12 +46,13 @@ class Animal:
         self.name = str(name)
         self.weight = float(weight)
         self.diet_type = diet_type
-    
+
     def run(self):
         return "vroom, vroom, I travel quickly"
 
     def eat(self, food):
         return "Huge fan of that " + str(food)
+
 
 class Sloth(Animal):
     def __init__(self, name, weight, diet_type, num_naps):
@@ -55,23 +61,21 @@ class Sloth(Animal):
 
     def say_something(self):
         return "This is sloths of typing"
-    
+
     def run(self):
         return "I am too slow to run"
-    
-
-
 
 if __name__ == '__main__':
-    num1 = Complex(3,5)
-    num2 = Complex(4,2)
+    num1 = Complex(3, 5)
+    num2 = Complex(4, 2)
     num1.add(num2)
     print(num1.r, num1.i)
 
     user1 = SocialMediaUser('Justin', 'Provo')
     user2 = SocialMediaUser('Nick', 'Logan', 200)
-    user3 = SocialMediaUser(name='Carl', location= 'Costa Rica', upvotes=1000)
+    user3 = SocialMediaUser(name='Carl', location='Costa Rica', upvotes=1000)
     user4 = SocialMediaUser('George Washington', 'Delaware', 2)
-    print("name: {}, is popular: {}. num upvotes: {}".format(user4.name, user4.is_popular(), user4.upvotes))
-    print('name: {}, is popular: {}, num upvotes: {}'.format(user3.name, user3.is_popular(), user3.upvotes))
-
+    print("name: {}, is popular: {}. num upvotes: {}".format(
+        user4.name, user4.is_popular(), user4.upvotes))
+    print('name: {}, is popular: {}, num upvotes: {}'.format(
+        user3.name, user3.is_popular(), user3.upvotes))
